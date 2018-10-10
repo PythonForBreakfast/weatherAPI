4c9c0df95755494b0fa78e42763ba60ea6dc6b2d
Michael Nathan <realmichaelnathan@protonmail.com>
2018-10-10 01:07:09 -0500

    Yet Another Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+    //Declare initial variables
     var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     var dtime = new Date();
@@ -6,6 +6,7 @@
     var month = months[ dtime.getMonth() ];
     var APPID = 'f2b53e7ee56cee418dca0659c75a0fb8';
 
+    //This function converts the unix time that we receive from the API.
     function convertTime(t) {
         var dt = new Date(t*1000);
         var hr = dt.getHours();
@@ -14,6 +15,7 @@
         return hr+ ':' + m.substr(-2);  
     }
 
+    //This function fires off when the "Get Weather" button is clicked. It calls the function that gets information from the API with the user input.
     function getWeather() {
         let zipCode = document.getElementById("zip").value;
 
@@ -23,13 +25,13 @@
             $("#container")
                 .css("display", "flex")
                 .hide()
-                .fadeIn("slow");
-                
+                .fadeIn("slow");  
         }
     }
     
+    //This function calls the API and updates the DOM with the information it receives. 
     function displayWeather(zipCode) {
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=" + APPID, function(data){
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&appid=" + APPID, function(data){
             $("#cityName").text(data.name);
             $("#icon").attr("src","images/" + data.weather[0].icon + ".png");
             $("#day").text(day);
@@ -42,5 +44,11 @@
             $("#lowTemp").html(Math.floor(data.main.temp_min * (9/5) -459.67) + "°F");
             $("#sunrise").html(convertTime(data.sys.sunrise));
             $("#sunset").html(convertTime(data.sys.sunset));
+
+            if (data.weather[0].icon[2] == "d") {
+                $("body").css("background-image","url('images/sunnysky2.jpg')");
+            } else {
+                $("body").css("background-image","url('images/nightsky.jpg')");
+            }
         });
     }
